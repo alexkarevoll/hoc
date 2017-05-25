ab199c42c2371c209402c90a172a44310c8f0078
alexkarevoll <alex.karevoll@gmail.com>
2017-05-25 15:27:48 -0700

    basic router setup

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>Sugar</li>
+        <li>Sugar</li>
+        <li>Sugar</li>
+      </ul>
+    </div>
+  );
+}
